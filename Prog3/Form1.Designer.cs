8b8b870e6e8c15c9c70c496c6e1e124aece3d168
Julius1990 <julius_weyl@gmx.net>
2014-04-26 12:31:00 +0200

    Design nochmal überarbeitet. Die einzelnen Elemente wachsen und schrumpfen jetzt dynamisch mit der Fenstergröße. Außerdem habe ich eine Menubar hinzugefügt. Mit dieser kann man bereits Bilder öffnen. Mann kann jpeg, gif, png, bmp und tif öffnen, der Rest wird ignoriert.

--- Prog3/Form1.Designer.cs
+++ Prog3/Form1.Designer.cs
@@ -28,127 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.allesPanel = new System.Windows.Forms.Panel();
-            this.infoPanel = new System.Windows.Forms.Panel();
-            this.fortschrittPanel = new System.Windows.Forms.Panel();
-            this.optionenPanel = new System.Windows.Forms.Panel();
-            this.pictureboxPanel = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.bildPicturebox = new System.Windows.Forms.PictureBox();
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
             this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.allesPanel.SuspendLayout();
+            this.öffnenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.bildOeffnenDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bildPicturebox)).BeginInit();
             this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // allesPanel
+            // splitContainer1
             // 
-            this.allesPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.allesPanel.AutoScroll = true;
-            this.allesPanel.AutoSize = true;
-            this.allesPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.allesPanel.Controls.Add(this.infoPanel);
-            this.allesPanel.Controls.Add(this.fortschrittPanel);
-            this.allesPanel.Controls.Add(this.optionenPanel);
-            this.allesPanel.Controls.Add(this.pictureboxPanel);
-            this.allesPanel.Controls.Add(this.menuStrip2);
-            this.allesPanel.Location = new System.Drawing.Point(12, 9);
-            this.allesPanel.MinimumSize = new System.Drawing.Size(100, 100);
-            this.allesPanel.Name = "allesPanel";
-            this.allesPanel.Size = new System.Drawing.Size(608, 437);
-            this.allesPanel.TabIndex = 0;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(20);
+            this.splitContainer1.Name = "splitContainer1";
             // 
-            // infoPanel
+            // splitContainer1.Panel1
             // 
-            this.infoPanel.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.infoPanel.AutoScroll = true;
-            this.infoPanel.AutoSize = true;
-            this.infoPanel.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.infoPanel.Location = new System.Drawing.Point(4, 384);
-            this.infoPanel.MinimumSize = new System.Drawing.Size(50, 50);
-            this.infoPanel.Name = "infoPanel";
-            this.infoPanel.Size = new System.Drawing.Size(439, 50);
-            this.infoPanel.TabIndex = 3;
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer3);
             // 
-            // fortschrittPanel
+            // splitContainer1.Panel2
             // 
-            this.fortschrittPanel.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.fortschrittPanel.AutoScroll = true;
-            this.fortschrittPanel.AutoSize = true;
-            this.fortschrittPanel.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.fortschrittPanel.Location = new System.Drawing.Point(448, 334);
-            this.fortschrittPanel.MinimumSize = new System.Drawing.Size(100, 100);
-            this.fortschrittPanel.Name = "fortschrittPanel";
-            this.fortschrittPanel.Size = new System.Drawing.Size(157, 100);
-            this.fortschrittPanel.TabIndex = 2;
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(592, 447);
+            this.splitContainer1.SplitterDistance = 405;
+            this.splitContainer1.TabIndex = 5;
             // 
-            // optionenPanel
+            // splitContainer3
             // 
-            this.optionenPanel.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.optionenPanel.AutoScroll = true;
-            this.optionenPanel.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.optionenPanel.Location = new System.Drawing.Point(448, 37);
-            this.optionenPanel.MinimumSize = new System.Drawing.Size(100, 100);
-            this.optionenPanel.Name = "optionenPanel";
-            this.optionenPanel.Size = new System.Drawing.Size(157, 291);
-            this.optionenPanel.TabIndex = 1;
+            this.splitContainer3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
-            // pictureboxPanel
+            // splitContainer3.Panel1
             // 
-            this.pictureboxPanel.AutoScroll = true;
-            this.pictureboxPanel.AutoSize = true;
-            this.pictureboxPanel.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureboxPanel.Location = new System.Drawing.Point(4, 37);
-            this.pictureboxPanel.MinimumSize = new System.Drawing.Size(100, 100);
-            this.pictureboxPanel.Name = "pictureboxPanel";
-            this.pictureboxPanel.Size = new System.Drawing.Size(439, 341);
-            this.pictureboxPanel.TabIndex = 0;
+            this.splitContainer3.Panel1.AutoScroll = true;
+            this.splitContainer3.Panel1.Controls.Add(this.bildPicturebox);
+            this.splitContainer3.Size = new System.Drawing.Size(405, 447);
+            this.splitContainer3.SplitterDistance = 339;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // bildPicturebox
+            // 
+            this.bildPicturebox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bildPicturebox.Image = ((System.Drawing.Image)(resources.GetObject("bildPicturebox.Image")));
+            this.bildPicturebox.Location = new System.Drawing.Point(0, 0);
+            this.bildPicturebox.Name = "bildPicturebox";
+            this.bildPicturebox.Size = new System.Drawing.Size(403, 337);
+            this.bildPicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bildPicturebox.TabIndex = 0;
+            this.bildPicturebox.TabStop = false;
             // 
             // menuStrip2
             // 
             this.menuStrip2.AutoSize = false;
-            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.None;
             this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.dateiToolStripMenuItem});
-            this.menuStrip2.Location = new System.Drawing.Point(4, 5);
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Margin = new System.Windows.Forms.Padding(2);
             this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(601, 24);
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(2);
+            this.menuStrip2.Size = new System.Drawing.Size(592, 24);
             this.menuStrip2.TabIndex = 4;
             this.menuStrip2.Text = "menuStrip2";
             // 
             // dateiToolStripMenuItem
             // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.öffnenToolStripMenuItem});
             this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
             this.dateiToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
             this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // öffnenToolStripMenuItem
+            // 
+            this.öffnenToolStripMenuItem.Name = "öffnenToolStripMenuItem";
+            this.öffnenToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.öffnenToolStripMenuItem.Text = "Öffnen";
+            this.öffnenToolStripMenuItem.Click += new System.EventHandler(this.öffnenToolStripMenuItem_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.splitContainer2.Size = new System.Drawing.Size(183, 447);
+            this.splitContainer2.SplitterDistance = 262;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // bildOeffnenDialog
+            // 
+            this.bildOeffnenDialog.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(632, 456);
-            this.Controls.Add(this.allesPanel);
+            this.ClientSize = new System.Drawing.Size(592, 471);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.allesPanel.ResumeLayout(false);
-            this.allesPanel.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bildPicturebox)).EndInit();
             this.menuStrip2.ResumeLayout(false);
             this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel allesPanel;
-        private System.Windows.Forms.Panel pictureboxPanel;
-        private System.Windows.Forms.Panel optionenPanel;
-        private System.Windows.Forms.Panel infoPanel;
-        private System.Windows.Forms.Panel fortschrittPanel;
         private System.Windows.Forms.MenuStrip menuStrip2;
         private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox bildPicturebox;
+        private System.Windows.Forms.ToolStripMenuItem öffnenToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog bildOeffnenDialog;
 
 
     }
--- Prog3/Form1.cs
+++ Prog3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,22 @@ namespace Prog3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void öffnenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Dialog anzeigen
+            DialogResult rs = bildOeffnenDialog.ShowDialog();
+            if (rs == DialogResult.OK)
+            {
+                //extrahiert die Endung des geöffneten Files
+                string ext = Path.GetExtension(bildOeffnenDialog.FileName);
+                //sortiert die NICHT Bildformate aus
+                if (ext == ".jpg" || ext == ".JPG" || ext == ".jpeg" || ext == ".png" || ext == ".gif" || ext == ".tif" || ext == ".bmp")
+                {
+                    bildPicturebox.Image = Image.FromFile(bildOeffnenDialog.FileName);
+                }
+            }
         }
     }
 }
